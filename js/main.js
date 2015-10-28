9d299acedfde2032d46543d1bb9cc729070932ae
Branden Cash <bcash@parchment.com>
2015-10-28 09:09:45 -0700

    improved position of pins

--- js/main.js
+++ js/main.js
@@ -1,13 +1,15 @@
 (function ($) {
+    var total = 0;
     var container = $('.dartboard-container').dartboard({}).on('slice-click', function (e, slice, origEvent) {
-        console.log(slice, origEvent);
-        var dart = $('<i class="fa fa-map-marker"></i>');
+        var dart = $('<i class="fa fa-map-marker"></i>').hide();
+        container.append(dart);
         dart.css({
             position: 'absolute',
-            top: origEvent.offsetY + 'px',
-            left: origEvent.offsetX + 'px'
-        })
-        container.append(dart);
+            top: (origEvent.offsetY - dart.outerHeight()) + 'px',
+            left: (origEvent.offsetX - (dart.outerWidth() / 2)) + 'px'
+        }).show();
+        total += slice.total;
+        console.log(total);
     });
 }(window.jQuery));
 
